d910cd4f527af1a4cdb209fefd77435060dea4d0
Emanuel Knecht <emanuel.knecht@scs.ch>
2021-10-28 15:54:08 +0200

    demo

---
+++ Assets/LookAtMouse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Physics.Raycast( Camera.main.ScreenPointToRay(Input.mousePosition), out var hit))
+        {
+            transform.LookAt(hit.point);
+        }
+    }
+}
